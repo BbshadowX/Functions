6a897c53daf08a402ae5d72bfa5536eb433f8df8
Bryan Crawford <bryancrawford14@gmail.com>
2023-05-03 23:08:00 -0400

    added discount chain psuedocode

---
+++ discount-chain/index.js
@@ -0,0 +1,4 @@
+//create a function that comes up with the total cost  of the transaction
+// if the customer has a subscription than they receive a 25% discount
+// if they have a cupon than give them an additional discount for $10
+//print your grand total is ${finalamount}
